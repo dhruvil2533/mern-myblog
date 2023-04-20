e0d5201a0077c24b68e2ff6d2ac5f5f6e7157f58
Dhruvil Modi <dhruvilmodi2503@gmail.com>
2023-04-20 23:54:42 +0530

    front-end url

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(
-  cors({ credentials: true, origin: "https://mern-myblog.netlify.app/" })
+  cors({ credentials: true, origin: "https://mern-myblog.netlify.app" })
 );
 app.use(express.json());
 app.use(cookieParser());
